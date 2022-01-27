7da158ebac0cfee7033c19c7a5a4b1af7ac40579
Woody <colorguitar@hotmail.com>
2022-01-27 17:46:42 +0800

    add create trello card

--- index.py
+++ index.py
@@ -2,6 +2,12 @@
 import json
 
 import requests
+from config import AUTHORIZATION
+
+
+# list_id = "61ae07345439df8afcbb15f2"  # "name": "商机线索池 -切入点待验证"
+# label: 'id': '61a9f4ef33ccc35e9209fc75', 'name': '新商机',
+# label: 'id': '61f14b9893957c737f7e8d8e', 'name': '来源：方案咨询问卷',
 
 
 def main_handler(event, context):
@@ -43,17 +49,39 @@ def main_handler(event, context):
             for option in question['options']:
                 channel += option['text'] + ';'
 
+    survey = '姓名：%s \n 微信：%s \n 电话：%s \n 邮箱：%s \n 公司：%s \n 职位：%s \n 需求：%s \n 方案：%s \n 渠道：%s' % (
+        name, wechat, phone, email, company, title, requirement, solution, channel)
     template = '''
             {
                 "msgtype": "markdown",
                 "markdown": {
-                    "content": "**方案咨询问卷有新数据了** \n 姓名：%s \n 微信：%s \n 电话：%s \n 邮箱：%s \n 公司：%s \n 职位：%s \n 需求：%s \n 方案：%s \n 渠道：%s"
+                    "content": "**方案咨询问卷有新数据了** \n %s"
                 }
             }
             '''
-    content = template % (name, wechat, phone, email, company, title, requirement, solution, channel)
+    content = template % survey
     r = requests.post('https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=81ef7bfa-0ff4-4850-86df-a8e272c8cafe',
                       data=content.encode('utf-8'))
     print(r.status_code)
     print(r.content)
+    create_trello_card(survey, name)
     return r.status_code
+
+
+def create_trello_card(survey, name):
+    headers = {'Authorization': AUTHORIZATION, 'Accept': 'application/json'}
+    template = '''{
+      "desc": "%s",
+      "idBoard": "61a9f4eff4d82d5399d26e70",
+      "idList": "61ae07345439df8afcbb15f2",
+      "idLabels": [
+        "61a9f4ef33ccc35e9209fc75",
+        "61f14b9893957c737f7e8d8e"
+      ],
+      "name": "方案咨询 - %s"
+    }'''
+    content = template % (survey, name)
+    r = requests.post('https://api.trello.com/1/cards?idList=61ae07345439df8afcbb15f2',
+                      headers=headers, data=content.encode('utf-8'))
+    print(r.status_code)
+    print(r.content)
